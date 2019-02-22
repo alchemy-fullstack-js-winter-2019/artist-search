2c96c1d8077c54d17ab370c1db5d473f91ffefc4
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-21 16:39:37 -0800

    Search component rendering in browser

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  // Redirect,
+  // Link 
+} from 'react-router-dom';
+import Search from '../src/components/Search';
 
 function App() {
   return (
-    <h1> Hello World </h1>
+    <Router>
+      < >
+        <Route exact path='/' component={Search} />
+        {/* <Route exact path='/about' component={About} /> */}
+        {/* <Route exact path='/artist/:id' component={Artist} /> */}
+        {/* <Route exact path='/work/:id' component={Song} /> */}
+        {/* <Redirect to='/home' /> */}
+      </>
+    </Router>
   );
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,1 +1,14 @@
-console.log('hello');
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function Search() {
+  return (
+    <h1>Sup Searchie</h1>
+  );
+}
+
+Search.propTypes = {
+
+}
+
+export default Search;
