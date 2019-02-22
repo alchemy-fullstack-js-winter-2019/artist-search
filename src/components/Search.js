6a7efad53c72d82bec6013931f788f6b30d079bc
Tyler Corbett <thetylercorbett@gmail.com>
2019-02-22 12:24:25 -0800

    going to lunch

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,28 +6,26 @@ export default class Search extends Component {
   state = {
     query: ''
   };
-
-
-  onSubmit = event => {
+  
+  handleSubmit = event => {
     event.preventDefault();
-    console.log('form submitted');
-    this.setState({ query: 'Nirvana' });
+    // this.handleChange(event);
+    console.log('form submitted', event);
   }
 
-  // handleChange = ({ target }) => {
-  //   // const { query } = this.state;
-  //   this.setState({ [target.name]: target.value }, () => {
-  //     console.log('input yo');
-  //   });
-  // }
+  handleChange = event => {
+    // const { query } = this.state;
+    console.log(event.target.value);
+    this.setState({ query: event.target.value });
+  }
 
   render() {
     const { query } = this.state;
     return (
       < >
       <h1>Sup Searchie</h1>
-      <form onSubmit={this.onSubmit}>
-        <input type="text" name="query" />
+      <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleChange} value={this.query} type="text" name="query" />
         <button>Yo</button>
       </form>
       {query && <Artists query={query}/>}
