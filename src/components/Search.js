4872d977f54d92f5ec5b66903ac457a65b15da04
Aaron Dennis <AMDennis1987@gmail.com>
2019-02-21 16:47:42 -0800

    search input added, console.logs

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,31 @@
 import React, { PureComponent } from 'react';
 
 export default class Search extends PureComponent {
+  state = {
+    artistText: ''
+  }
+  handleSearch = event => {
+    console.log(this.state.artistText);
+    event.preventDefault();
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
   render() {
+    const { artistText } = this.state;
     return (
+    <>
       <h1>Search!!!</h1>
+      <form onSubmit={this.handleSearch}>
+        <label>
+          Search Name:
+          <input type="text" name="artistText" value={artistText} onChange={this.handleChange}></input>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </> 
     );
   }
 }
