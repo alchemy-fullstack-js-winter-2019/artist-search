e51a3f0b3a40e8f478a138a42331bf2c7c4d47f5
Marcy Silverman <marcysilve225@gmail.com>
2019-02-22 16:56:12 -0800

    list of songs works

--- src/components/songs/Song.js
+++ src/components/songs/Song.js
@@ -1,1 +1,17 @@
-console.log('hello');
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Song({ works }) {
+  const listOfSongs = works.map(work => {
+    return <li key={work.id} song={work}> {work.title} </li>;
+  });
+  return (
+    <h1> {listOfSongs} </h1>
+  );
+}
+
+Song.propTypes = {
+  works: PropTypes.array
+};
+
+export default Song;
--- src/components/songs/Songs.js
+++ src/components/songs/Songs.js
@@ -1,33 +1,37 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-// import Song from './Song';
+import Song from './Song';
 import { getArtistSongs } from '../../services/getArtistsApi';
 
 
-class Songs extends PureComponent {
+export default class Songs extends PureComponent {
+  state = {
+    works: []
+  };
+
     static propTypes = {
       match: PropTypes.shape({ 
         params: PropTypes.shape({ 
           id: PropTypes.string
-        })
+        })  
       })
     };
    
     getSongs = () => {
       getArtistSongs(this.props.match.params.id)
         .then(response => {
-          console.log('response', response);
+          this.setState({ works: response.works });
         });
     }
     render() {
+      const { works } = this.state;
       console.log(this.props.match.params.id);
       return (
-        <ul>
-          <button onClick={this.getSongs}> Songs </button>
-        </ul>
+        <>
+        <button onClick={this.getSongs}> Get Songs</button>
+        {works && <Song works={works} /> }
+        </>
       );
     }
   
 }
-
-export default Songs;
