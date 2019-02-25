042d48542341540bb089941bcde186d75153a167
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-25 15:47:26 -0800

    added enzyme dependencies and setup, and Songs button click test

--- src/components/songs/Songs.test.js
+++ src/components/songs/Songs.test.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Songs from './Songs';
 
+jest.mock('../../services/getArtistsApi');
+
 describe('Songs', () => {
-  it('matches a snapshot', () => {
-    // const works = [
-    //   {
-    //     id: '02b2de82-9f31-3683-b112-419e1b86b1c5',
-    //     title: 'Beans'
-    //   },
-    //   {
-    //     id: '05ad4550-a38e-3cda-972c-900b3d335724',
-    //     title: 'Carmen: Acte I, No. 5. Havanaise : « L’amour est un oiseau rebelle » (Carmen, chœur)'
-    //   }
-    // ];
-    const tree = renderer.create(
-      // <Songs works={works}/>
-      <Songs />
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+  it('simulates click events', done => {
+    const match = { params: { id: 'Beans' } };
+    const wrapper = shallow(<Songs match={match}/>);
+    expect(wrapper.find('button').simulate('click'));
+    setTimeout(() => {
+      expect(wrapper.state().works).toHaveLength(25);
+      done();
+    }, 500);
   });
 });
