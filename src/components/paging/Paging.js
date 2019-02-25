42e67245b04f6ba89a5cf19a2345f11cde6fbbeb
Tyler Corbett <thetylercorbett@gmail.com>
2019-02-25 15:32:13 -0800

    styling and reformatting to paging

--- src/components/paging/Paging.js
+++ src/components/paging/Paging.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Paging.css';
 
 function Paging({ totalPages, currentPage, increaseCount, decreaseCount }) {
   return (
-    <h5>
-      <button onClick={increaseCount}>Next</button>
+    <h5 className={styles.Paging}>
+      <button onClick={decreaseCount}>Prev</button>
       <span>Current Page: {currentPage}</span>
       <span>Total Pages: {totalPages}</span>
-      <button onClick={decreaseCount}>Prev</button>
+      <button onClick={increaseCount}>Next</button>
     </h5>
   );
 }
