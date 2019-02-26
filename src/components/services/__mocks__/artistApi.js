0e4741ede92cbea2871e6defa3fa1172bf22a044
Shaba9 <shaba92@gmail.com>
2019-02-25 16:57:13 -0800

    ArtistDetail test passes

--- src/components/services/__mocks__/artistApi.js
+++ src/components/services/__mocks__/artistApi.js
@@ -1,12 +1,20 @@
-export const getArtists = ({ page, search }) => {
-  return fetch(`http://musicbrainz.org/ws/2/artist?query=${search}&fmt=json&offset=${page}`, {
-    headers: {
-      'origin': null
-    }
-  })
-    .then(res => res.json())
-    .then(json => ({
-      totalPages: Math.ceil(json.count / 25),
-      results: json.artists
-    }));
+const results = [
+  {
+    id: '1',
+    title: 'song1'
+  },
+  {
+    id: '2',
+    title: 'song2'
+  },
+  {
+    id: '3',
+    title: 'song3'
+  }
+];
+
+export const getArtistDetail = () => {
+  return Promise.resolve({
+    results
+  });
 };
