cb6c268c08072707e6924349a141bf352a02f3bd
Sophie Graham <sophiergraham1@gmail.com>
2019-03-08 13:34:57 -0800

    page counter working!

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 import Search from './Search';
+import Paging from './paging/Paging';
 
 export default class App extends Component {
+  state = {
+    currentPage: 2,
+    totalPages: 2
+  };
+
+  incrementCurrentPage = () => {
+    this.setState(state => ({ currentPage: state.currentPage + 1 }));
+  };
+
+  decrementCurrentPage = () => {
+    this.setState(state => ({ currentPage: state.currentPage - 1 }));
+  };
+
+  updateTotalPages = updatedPages => {
+    this.setState({ totalPages: updatedPages });
+  };
+
   render() { 
+    const { currentPage, totalPages } = this.state;
+
     return (
-      <Search />      
+      <>
+        <Paging
+          currentPage={currentPage}
+          totalPages={totalPages}
+          increment={this.incrementCurrentPage}
+          decrement={this.decrementCurrentPage}
+        />
+        <Search
+          page={currentPage}
+          updateTotalPages={this.updateTotalPages}
+        />   
+      </>   
     );
   }
 }
